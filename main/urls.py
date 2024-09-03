40a298a8385e5fb23a20c8ba101f2a2bd86d139f
b.kubulbekov <b.kubulbekov@tanto-s.ru>
2024-09-04 01:22:06 +0600

    finish

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from block.views import invitation_view
 
@@ -8,3 +10,13 @@ urlpatterns = [
     path('invite/<str:token>/', invitation_view, name='invitation_view'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT,
+    )
